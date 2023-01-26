6aad2ba2a590ac7065415b5d9e08bb49e29b6d82
Stephen J. Carnam <steve@steveorevo.com>
2023-01-25 23:59:30 -0800

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -14,10 +14,10 @@ $hcpp->register_uninstall_script( dirname(__FILE__) . '/uninstall' );
 // When a new domain is created, allocate a port for our NodeJS based application
 $hcpp->add_action( 'pre_add_web_domain_backend', function( $args ) {
     global $hcpp;
-    $name = 'nodeapp';
+    $name = 'nodeapp_port';
     $user = $args[0];
     $domain = $args[1];
     $port = $hcpp->allocate_port( $name, $user, $domain );
-    $hcpp->log( "Allocated port $port for $user/$domain" );
+    $hcpp->log( "Allocated $name on port $port for $user/$domain" );
     return $args;
 });
