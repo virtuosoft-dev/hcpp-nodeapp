f173241e18332adc01b5b397e0f4637fc2b3ac0a
Steveorevo <steve@steveorevo.com>
2023-01-29 11:21:13 -0800

    Support action API

--- nginx/NodeApp.sh
+++ nginx/NodeApp.sh
@@ -8,11 +8,18 @@ if (count( $argv ) < 6) {
     echo "Usage: <user> <domain> <ip> <home> <docroot>\n";
     exit( 1 );
 }else{
-    $user = $argv[1];
-    $domain = $argv[2];
-    $ip = $argv[3];
-    $home = $argv[4];
-    $docroot = $argv[5];
+    array_shift( $argv ); // remove the script path
+
+    // Allow override of the arguments
+    require_once( '/usr/local/hestia/web/pluginable.php' );
+    global $hcpp; 
+    $argv = $hcpp->do_action( 'pre_nodeapp_template', $argv );
+
+    $user = $argv[0];
+    $domain = $argv[1];
+    $ip = $argv[2];
+    $home = $argv[3];
+    $docroot = $argv[4];
 }
 
 // Utility string function
@@ -43,12 +50,15 @@ if ( !file_exists( $docroot ) ) {
             chgrp( $docroot . '/' . $file->getFilename(), $user );
         }
     }
+    $argv = $hcpp->do_action( 'copy_nodeapp_files', $argv );
 
     // Install the app.js dependencies
     $cmd = 'runuser -l ' . $user . ' -c "cd \"' . $docroot . '\" && source /opt/nvm/nvm.sh && npm install"';
+    $cmd = $hcpp->do_action( 'install_nodeapp_dependencies', $cmd );
     shell_exec( $cmd );
 }
 
 // Restart the nodeapp service for the domain
 $cmd = 'runuser -l ' . $user . ' -c "cd \"' . $docroot . '\" && source /opt/nvm/nvm.sh && pm2 delete app.config.js && pm2 start app.config.js"';
+$cmd = $hcpp->do_action( 'restart_nodeapp_services', $cmd );
 shell_exec( $cmd );
