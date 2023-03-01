a601e12d6add4fac22e954f2f2eafa56b88c2b59
Stephen J Carnam <steveorevo@gmail.com>
2023-02-28 18:27:55 -0800

    consolidate, support debug

--- nodeapp/app.config.js
+++ nodeapp/app.config.js
@@ -1,10 +1,10 @@
 /**
- * Get compatible PM2 app object with details and automatic support for .nvmrc, 
+ * Get compatible PM2 app config object with automatic support for .nvmrc, 
  * port allocation, and debug mode.
  */
 module.exports = {
     apps: (function() {
-        let nodeapp = [require(__dirname + '/nodeapp.js')(__filename)];
+        let nodeapp = [require('/usr/local/hestia/plugins/nodeapp/nodeapp.js')(__filename)];
         return nodeapp;
     })()
 }
